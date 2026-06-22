b89dfd6ffdc32756276dda5c104df8c5185e8e40
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2026-06-22 19:59:24 +0500

    feat: add error handling for Express application loading in serverless entry point

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
-const app = require('../fileflip_custom_index/server');
+let app;
+let loadError = null;
 
-module.exports = app;
+try {
+    app = require('../fileflip_custom_index/server');
+} catch (err) {
+    loadError = err;
+}
+
+module.exports = (req, res) => {
+    if (loadError) {
+        return res.status(500).json({
+            error: "Failed to load Express application",
+            message: loadError.message,
+            stack: loadError.stack
+        });
+    }
+    return app(req, res);
+};
+
 
